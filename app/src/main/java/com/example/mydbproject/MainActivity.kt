97f3b5fa7d885195296696c918d866480b00d61c
Admin <makansari@gmail.com>
2021-12-16 07:48:19 +0530

    view Model and Live Data

--- app/src/main/java/com/example/mydbproject/MainActivity.kt
+++ app/src/main/java/com/example/mydbproject/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.mydbproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.GlobalScope
@@ -11,11 +13,39 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
 
     lateinit var database : StudentDatabase
+    lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        database = Room.databaseBuilder(this,StudentDatabase::class.java,
+       /* var counter = 0
+        button.setOnClickListener {
+            counter++
+            textView.setText(""+ counter)
+        }*/
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.startTimer()
+
+        viewModel.sec.observe(this, Observer {
+            textView.text = it.toString()
+        })
+
+        viewModel.countfinish.observe(this, Observer {
+            textView.text = it.toString()
+        })
+
+       /* textView.text = viewModel.numb.toString()
+
+        button.setOnClickListener {
+            viewModel.incNumber()
+            textView.text = viewModel.numb.toString()
+        }*/
+
+
+
+
+
+    /*    database = Room.databaseBuilder(this,StudentDatabase::class.java,
             "mystudendb").fallbackToDestructiveMigration().build()
         button.setOnClickListener {
             GlobalScope.launch {
@@ -34,6 +64,6 @@ class MainActivity : AppCompatActivity() {
                     Log.i("mytag",result)
                 }
             }
-        }
+        }*/
     }
 }
